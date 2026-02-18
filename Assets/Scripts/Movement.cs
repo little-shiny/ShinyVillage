27232647f69fdca535cd39903b0e207d3105f739
Cristina ❤️ <mim0xina@gmail.com>
2026-02-19 00:12:44 +0100

    Creación de la clase de movimiento del personaje. se mueve utilizando la clase Input Manager y las teclas de dirección (WASD)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
 
-public class Movement
+/*
+* Clase que representa elmovimiento del sprite del jugador
+* Propiedades: Speed (velocidad)
+**/
+
+public class Movement : MonoBehaviour
 {
-    
+    public float speed;
+
+    // Se necesita obtener información de la entrada del usuario y actualizarse constantemente
+
+    private void Update()
+    {   
+        // Variable para almacenar el eje horizontal
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Variable que almacena la posicion (coordenadas)
+        Vector3 direction = new Vector3(horizontal,vertical);
+
+        transform.position += direction * speed * Time.deltaTime;
+        // Time.deltaTime es la diferencia de tiempo entre la última vez que se actualizó
+
+
+    }
 }
