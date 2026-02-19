ae822ccb91fc64b775ac77027b3839b62a518934
Cristina ❤️ <mim0xina@gmail.com>
2026-02-19 14:15:26 +0100

    Corrección del problema de los ejes

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,6 +9,8 @@ public class Movement : MonoBehaviour
 {
     public float speed;
 
+    public Animator animator; // Representa la animación del sprite
+
     // Se necesita obtener información de la entrada del usuario y actualizarse constantemente
 
     private void Update()
@@ -20,9 +22,35 @@ public class Movement : MonoBehaviour
         // Variable que almacena la posicion (coordenadas)
         Vector3 direction = new Vector3(horizontal,vertical);
 
+        direction = direction.normalized;
+
+        animateMovement(direction);
+
         transform.position += direction * speed * Time.deltaTime;
         // Time.deltaTime es la diferencia de tiempo entre la última vez que se actualizó
 
 
+    }
+
+    void animateMovement(Vector3 direction)
+    {
+        if(animator != null)
+        {
+            if(direction.magnitude  > 0) // Siempre debería ser positivo porque es la longitud del vector, y 
+            // Si el modulo es 0 significa que no hay movimiento del sprite
+            {
+                // como es positivo, nos movemos
+                animator.SetBool("isMoving", true); // le decimos que hay movimiento a la animación
+
+                // Le decimos la dirección a la que se mueve     
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            
+            } 
+            else
+            {  // Como es cero, no nos movemos
+                animator.SetBool("isMoving", false);
+            } 
+        }
     }
 }
