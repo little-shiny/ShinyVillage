65e21d4eb358b17e6d2caf3b591f8b8c1903464e
Cristina ❤️ <mim0xina@gmail.com>
2026-02-21 21:12:57 +0100

    Collectables funcionando. El jugador recoge la semilla y la propiedad aumenta al recogerla

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -8,15 +8,15 @@ public class Collectable : MonoBehaviour
     // Función que detecta el trigger del collider del item. el problema es que no sabemos que collider es y si es el del player
     // Para ello se necesita otro script para saber que viene del player (Script Player.cs)
 
-    private void onTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Player player = collision.getComponent<Player>(); // Busca si existe el script del componente Player y si colisiona con el item
+        Player player = collision.GetComponent<Player>(); // Busca si existe el script del componente Player y si colisiona con el item
 
         // Si player existe
 
         if (player)
         {
-            player.numCarrotSeed++;
+            player.numOnionSeed++;
 
             //Ahora se elimina el item porque se ha "recogido"
             Destroy(this.gameObject); // Pasamos el item para eliminarlo
