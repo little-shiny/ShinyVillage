b5e94cea86b5dc7ed7613174a7b1c8d3c299fe97
Cristina ❤️ <mim0xina@gmail.com>
2026-02-21 01:43:36 +0100

    Creación del script de Player y se completa el que se habia empezado para recoger el item

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -6,6 +6,21 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     // Función que detecta el trigger del collider del item. el problema es que no sabemos que collider es y si es el del player
-    // Para ello se necesita otro script
+    // Para ello se necesita otro script para saber que viene del player (Script Player.cs)
+
+    private void onTriggerEnter2D(Collider2D collision)
+    {
+        Player player = collision.getComponent<Player>(); // Busca si existe el script del componente Player y si colisiona con el item
+
+        // Si player existe
+
+        if (player)
+        {
+            player.numCarrotSeed++;
+
+            //Ahora se elimina el item porque se ha "recogido"
+            Destroy(this.gameObject); // Pasamos el item para eliminarlo
+        }
+    }
     
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Creamos un contador para el jugador del numero de items
+    public int numOnionSeed = 0;
+}
