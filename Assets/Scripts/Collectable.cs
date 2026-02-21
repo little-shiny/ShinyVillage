5880e0781932043e0613f76f1fc00b14f9ea08c0
Cristina ❤️ <mim0xina@gmail.com>
2026-02-21 01:35:36 +0100

    Se emieza con el script para recoger objetos. surje el problema de saber que collider es el del jugador

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+// Se necesita que el player se ponga encima del collectable
+// Se añade el collectable al player
+// se elimina el collectable de la escena
+
+public class Collectable : MonoBehaviour
+{
+    // Función que detecta el trigger del collider del item. el problema es que no sabemos que collider es y si es el del player
+    // Para ello se necesita otro script
+    
+}
