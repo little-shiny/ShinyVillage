42407017cd458f575ca5957dee9cd2a4f706e312
Cristina ❤️ <mim0xina@gmail.com>
2026-03-01 20:54:12 +0100

    Prefabs de botones de aceptar/cancelar modificaciones en el lobbyPannel que no permitian hacer click en los botones porque había un desfase entre los pivots de los paneles y el canvas

--- Assets/Scripts/UI/InputFieldCustomText.cs
+++ Assets/Scripts/UI/InputFieldCustomText.cs
@@ -9,13 +9,13 @@ public class InputFieldCustomText : MonoBehaviour
     private const string GAME_NAME_TEXT = "Nombre de la partida...";
     private const string PLAYER_NAME_TEXT = "Tu nombre...";
 
-    void Start()
+    void Update() 
     {
-        gameNameField.interactable = true;
+     gameNameField.interactable = true;
         playerNameField.interactable = true;
 
         gameNameField.text = GAME_NAME_TEXT;
-        playerNameField.text = PLAYER_NAME_TEXT;
+        playerNameField.text = PLAYER_NAME_TEXT;   
     }
 
 }
