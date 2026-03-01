a4664dd304ee0bf49e102b7352ff95f48a9cce18
Cristina ❤️ <mim0xina@gmail.com>
2026-03-01 09:08:53 +0100

    Creación del script JSONManager para el manejo de datos de guardado maneja strings y guarda en un archivo JSON

---
+++ Assets/Scripts/JSONManager.cs
@@ -0,0 +1,51 @@
+using System.IO;
+using UnityEngine;
+//Script que controla y maneja el guardado y carga de datos de las partidas del juego
+[System.Serializable]
+public class PlayerData
+{
+    public string playerName;
+}
+public class JSONManager : MonoBehaviour
+{
+    //Ruta de guardado de los JSON con los datos. Guardará los datos en la carpeta Assets
+    private string filePath;
+
+    void Start()
+    {
+        filePath = Application.dataPath + "/Save/UserData.json";
+    }
+
+    //Método que guarda los datos del objeto PlayerData en un archivo JSON con la librería propia de Unity. 
+    //Almacena en un string los datos del archivo JSON
+    public void SaveData()
+    {
+        PlayerData player = new PlayerData();
+        player.playerName = "ExampleUsername"; //Usamos un usuario de prueba 
+
+        string json = JsonUtility.ToJson(player, true);
+        File.WriteAllText(filePath, json);
+        //Debug
+        Debug.Log("Data saved" + json);
+    }
+
+    //Función que carga los datos del Json. Necesitamos comprobar si el fichero ya existe porque si no lanza excepción
+    public void LoadData()
+    {
+        if (File.Exists(filePath)) //Si existe el archivo
+        {
+            string json = File.ReadAllText(filePath);
+            //Ahora convertimos el string del json en un objeto playerData de nuevo
+            PlayerData loadedPlayer = JsonUtility.FromJson<PlayerData>(json);
+
+            //debug
+            Debug.Log("Loaded Data" + loadedPlayer.playerName);
+        }
+        else
+        {
+            //debug
+            Debug.Log("No saved data found");
+        }
+    }
+
+}
